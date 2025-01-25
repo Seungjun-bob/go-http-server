00e1c20b450fb05f87fe986ffc076cf4548208a2
Seungjun <rkdtmdwns0105@gmail.com>
2025-01-25 17:02:54 +0900

    - Rest Client 생성 - GET, POST 예시 추가가

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,6 +14,38 @@ func main() {
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, this is the /hello endpoint!")
+	})
+
+	// get 요청 처리
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "Guest"
+		}
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+
+	// post 요청 처리
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		// POST 메소드인지 확인
+		if r.Method == http.MethodPost {
+			// 요청 본문에서 JSON 데이터를 읽어오기
+			var data struct {
+				Name string `json:"name"`
+			}
+
+			// JSON 디코딩
+			err := json.NewDecoder(r.Body).Decode(&data)
+			if err != nil {
+				http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+				return
+			}
+
+			// 성공적으로 처리된 경우 응답
+			fmt.Fprintf(w, "Received name: %s", data.Name)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	})
 
 	// 2. 서버 시작
